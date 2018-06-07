e49fee71218e3f5059e36b6fd577437582c1f381
HelloKittycoder <shucheng2015@outlook.com>
2018-06-07 17:31:25 +0800

    Update initDataBase.bat

--- commonOperations/initDataBase/initDataBase.bat
+++ commonOperations/initDataBase/initDataBase.bat
@@ -1,2 +1,7 @@
+--连接本地数据库（用户名--jbit 密码--jbit  sid--orcl  主机名--localhost 端口号--1521），并执行sql
+--完整写法  sqlplus "jbit/jbit@localhost:1521/orcl" @initDataBase.sql  下面是简便写法
 sqlplus "jbit/jbit@orcl" @initDataBase.sql
+
+--连接远程数据库（用户名--invest_jbit 密码--invest_jbit  sid--orcl  主机名--192.168.1.200 端口号--1521），并执行sql
+--sqlplus "invest_jbit/invest_jbit@192.168.1.200:1521/orcl" @initDataBase.sql
 pause
